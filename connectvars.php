2bab3bc43d029340227ea10478a595c2915adba6
Nicole Campagna <nikicampagna17@gmail.com>
2020-04-22 11:24:56 -0400

    deleting files for merge

--- connectvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Define database connection constants
-   define('DB_HOST', 'localhost');
-   define('DB_USER', 'ncampagna'); //replace this
-   define('DB_PASSWORD', 'Meme.6363'); //edit this
-   define('DB_NAME', 'ncampagna'); //edit this
-?>
